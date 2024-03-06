198f7a5442b2a3b5a7df6c58920721c9e8c50c5f
gagik <gagik.abovyan>
2024-03-06 16:35:21 +0400

    Add system hint block

---
+++ app/src/main/java/com/bignerdranch/android/launcherapp/customView/LineView.kt
@@ -0,0 +1,37 @@
+package com.bignerdranch.android.launcherapp.customView
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class LineView : View {
+    private val linePaint = Paint()
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        linePaint.color = 0xFFFF0000.toInt()
+        linePaint.strokeWidth = 2f
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        val startX = (width * 0.1).toFloat()
+        val endX = (width * 0.9).toFloat()
+        val y = height.toFloat()
+        canvas.drawLine(startX, y, endX, y, linePaint)
+    }
+
+}
--- app/src/main/java/com/bignerdranch/android/launcherapp/ui/NotificationActivity.kt
+++ app/src/main/java/com/bignerdranch/android/launcherapp/ui/NotificationActivity.kt
@@ -1,21 +1,73 @@
 package com.bignerdranch.android.launcherapp.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.launcherapp.customView.CustomNotificationPopup
 import com.bignerdranch.android.launcherapp.databinding.ActivityNotificationBinding
 
 class NotificationActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityNotificationBinding
+    private var isRectangleExpanded = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNotificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.showPopupBtn.setOnClickListener {
-            showPopup(binding.root)
+        binding.imageButton.setOnClickListener {
+            val scaleXAnimator = createScaleXAnimator()
+            val rotateAnimator = createRotateAnimator()
+
+            AnimatorSet().apply {
+                playTogether(scaleXAnimator, rotateAnimator)
+                start()
+            }
+
+            toggleHintVisibility()
+            isRectangleExpanded = !isRectangleExpanded
         }
+    }
+
+    private fun updateUIHintText(hintTitle: String, descrHint: String, titleColor: Int, descrColor: Int) {
+        binding.hintTitleTV.text = hintTitle
+        binding.hintDescrTV.text = descrHint
+        binding.hintTitleTV.setTextColor(getColor(titleColor))
+        binding.hintDescrTV.setTextColor(getColor(descrColor))
+    }
+
+    private fun createScaleXAnimator(): ValueAnimator {
+        val startScaleX = if (isRectangleExpanded) 1f else 0.25f
+        val endScaleX = if (isRectangleExpanded) 0.25f else 1f
+
+        return ValueAnimator.ofFloat(startScaleX, endScaleX).apply {
+            interpolator = AccelerateDecelerateInterpolator()
+            duration = 500
+            addUpdateListener {
+                val value = it.animatedValue as Float
+                binding.rectangleLayout.scaleX = value
+                binding.imageButton.translationX = (binding.rectangleLayout.width - binding.imageButton.width) / 2 * (1 - value)
+            }
+        }
+    }
+
+    private fun createRotateAnimator(): ObjectAnimator {
+        val rotationAngle = if (isRectangleExpanded) 0f else 180f
+
+        return ObjectAnimator.ofFloat(binding.imageButton, "rotation", rotationAngle).apply {
+            interpolator = AccelerateDecelerateInterpolator()
+            duration = 500
+        }
+    }
+
+    private fun toggleHintVisibility() {
+        binding.hintTitleTV.visibility = if (isRectangleExpanded) View.INVISIBLE else View.VISIBLE
+        binding.hintDescrTV.visibility = if (isRectangleExpanded) View.INVISIBLE else View.VISIBLE
     }
 
     private fun showPopup(view: View) {
