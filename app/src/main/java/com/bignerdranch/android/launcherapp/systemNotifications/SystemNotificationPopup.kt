36b9686dcf8ba26a4445c78281e5b4b2dbe9bc25
David Melikyan <david@dynamic.am>
2024-03-08 00:16:28 +0400

    Minor changes

--- app/src/main/java/com/bignerdranch/android/launcherapp/systemNotifications/SystemNotificationPopup.kt
+++ app/src/main/java/com/bignerdranch/android/launcherapp/systemNotifications/SystemNotificationPopup.kt
@@ -12,17 +12,19 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.bignerdranch.android.launcherapp.R
 import com.bignerdranch.android.launcherapp.databinding.PopupSystemNotificationBinding
+import java.util.LinkedList
+import java.util.Queue
+import java.util.Stack
 
 internal class SystemNotificationPopup(
     view: View,
-    private val timer: Long = MILLIS_IN_FUTURE,
+    private val timer: Long,
     private val type: SystemNotificationTypes,
-    private val description: String = "",
-    private val onActionClicked: (() -> Unit)? = null
+    private val description: String,
+    private val onActionClicked: (() -> Unit)?,
+    private val onNotificationDismissed: () -> Unit,
 ) : PopupWindow() {
     companion object {
-        private const val MILLIS_IN_FUTURE = 20000L
-        private const val COUNTDOWN_TIMER_INTERVAL = 100L
         private const val TAG = "+++SystemNotificationPopup"
     }
 
@@ -37,6 +39,9 @@ internal class SystemNotificationPopup(
         width = ViewGroup.LayoutParams.MATCH_PARENT
         animationStyle = R.style.PopupAnimation
         showAtLocation(view, Gravity.BOTTOM, 0, 0)
+        setOnDismissListener {
+            onNotificationDismissed.invoke()
+        }
 
         val alphaAnimation = AlphaAnimation(0f, 1f)
         alphaAnimation.duration = 500
@@ -66,16 +71,17 @@ internal class SystemNotificationPopup(
     }
 
     private fun startLoading() {
-        countDownTimer = object : CountDownTimer(timer, COUNTDOWN_TIMER_INTERVAL) {
-            override fun onTick(millisUntilFinished: Long) {
-                val progress = 100 - (millisUntilFinished.toFloat() / timer * 100).toInt()
-                binding.progressBar.setProgress(progress, true)
-            }
+        countDownTimer =
+            object : CountDownTimer(timer, SystemNotificationBuilder.COUNTDOWN_TIMER_INTERVAL) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val progress = 100 - (millisUntilFinished.toFloat() / timer * 100).toInt()
+                    binding.progressBar.setProgress(progress, true)
+                }
 
-            override fun onFinish() {
-                dismiss()
+                override fun onFinish() {
+                    dismiss()
+                }
             }
-        }
         countDownTimer?.start()
     }
 
@@ -94,3 +100,37 @@ internal class SystemNotificationPopup(
     }
 
 }
+
+class SystemNotificationBuilder private constructor() {
+    companion object {
+        const val MILLIS_IN_FUTURE = 20000L
+        const val COUNTDOWN_TIMER_INTERVAL = 100L
+
+        private var instance: SystemNotificationBuilder? = null
+
+        fun getInstance(): SystemNotificationBuilder {
+            if (instance == null) {
+                instance = SystemNotificationBuilder()
+            }
+
+            return instance!!
+        }
+    }
+
+    private val notifications = LinkedList<SystemNotificationItem>()
+    private var showingNotifications = 0
+
+    fun showNotification(notification: SystemNotificationItem) {
+        notifications.add(notification)
+        if (showingNotifications < 1) {
+
+        }
+    }
+}
+
+data class SystemNotificationItem(
+    private val timer: Long = SystemNotificationBuilder.MILLIS_IN_FUTURE,
+    private val type: SystemNotificationTypes,
+    private val description: String,
+    private val onActionClicked: (() -> Unit)? = null,
+)
